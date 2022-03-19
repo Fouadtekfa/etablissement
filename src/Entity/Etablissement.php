b42c1c2b9dee49ad0911a1633bea726daf20d8ac
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-03-19 14:20:38 +0100

    creation d'entite etablissement

---
+++ src/Entity/Etablissement.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtablissementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtablissementRepository::class)]
+class Etablissement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $appellation_officielle;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $denomination_principale;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $secteur;
+
+    #[ORM\Column(type: 'float')]
+    private $latitude;
+
+    #[ORM\Column(type: 'float')]
+    private $longitude;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $adresse;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $departement;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $commune;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $region;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $academie;
+
+    #[ORM\Column(type: 'date')]
+    private $date_ouverture;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAppellationOfficielle(): ?string
+    {
+        return $this->appellation_officielle;
+    }
+
+    public function setAppellationOfficielle(string $appellation_officielle): self
+    {
+        $this->appellation_officielle = $appellation_officielle;
+
+        return $this;
+    }
+
+    public function getDenominationPrincipale(): ?string
+    {
+        return $this->denomination_principale;
+    }
+
+    public function setDenominationPrincipale(string $denomination_principale): self
+    {
+        $this->denomination_principale = $denomination_principale;
+
+        return $this;
+    }
+
+    public function getSecteur(): ?string
+    {
+        return $this->secteur;
+    }
+
+    public function setSecteur(string $secteur): self
+    {
+        $this->secteur = $secteur;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): self
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getDepartement(): ?string
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(string $departement): self
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+
+    public function getCommune(): ?string
+    {
+        return $this->commune;
+    }
+
+    public function setCommune(string $commune): self
+    {
+        $this->commune = $commune;
+
+        return $this;
+    }
+
+    public function getRegion(): ?string
+    {
+        return $this->region;
+    }
+
+    public function setRegion(string $region): self
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    public function getAcademie(): ?string
+    {
+        return $this->academie;
+    }
+
+    public function setAcademie(string $academie): self
+    {
+        $this->academie = $academie;
+
+        return $this;
+    }
+
+    public function getDateOuverture(): ?\DateTimeInterface
+    {
+        return $this->date_ouverture;
+    }
+
+    public function setDateOuverture(\DateTimeInterface $date_ouverture): self
+    {
+        $this->date_ouverture = $date_ouverture;
+
+        return $this;
+    }
+}
