d85306779cf512d0e32f2dc9286714dcf1e0ec5e
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-03-19 21:57:25 +0100

    creation de base pour la table des donnes et redirection a un etablissement precis

--- src/Controller/EtablissementsController.php
+++ src/Controller/EtablissementsController.php
@@ -9,11 +9,42 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EtablissementsController extends AbstractController
 {
-    #[Route('/etablissements', name: 'app_etablissements', methods: 'GET')]
+    #[Route('/etablissements', name: 'etablissements', methods: 'GET')]
     public function index(): Response
     {
+        $etablissements = [
+            [
+                "id" => 1,
+                "appellation_officielle" => "CAO",
+                "denomination_principale" => "Denom",
+                "secteur" => "Agricole",
+                "latitude" => "23,405",
+                "longitude" => "39,56",
+                "adresse" => "Le havre",
+                "departement" => "Seine Maritime",
+                "commune" => "Jsp",
+                "region" => "Normandie",   
+                "academie" => "Normand",
+                "date_ouverture" => "22/02/2022"   
+            ],
+            [
+                "id" => 2,
+                "appellation_officielle" => "FGH",
+                "denomination_principale" => "Denom2",
+                "secteur" => "Aviateur",
+                "latitude" => "35,405",
+                "longitude" => "86,46",
+                "adresse" => "Caen",
+                "departement" => "Calvados",
+                "commune" => "com",
+                "region" => "Normandie",   
+                "academie" => "Mont",
+                "date_ouverture" => "10/01/2020"   
+            ]
+        ];
+
         return $this->render('etablissements/index.html.twig', [
-            'controller_name' => 'EtablissementsController',
+            'etablissements' => $etablissements
         ]);
     }
 
