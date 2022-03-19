b0ddcf1bbe9148a1d9742515fd0da22debd98a9d
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-03-19 21:07:30 +0100

    route pour un etablissement individuel

--- src/Controller/EtablissementsController.php
+++ src/Controller/EtablissementsController.php
@@ -9,13 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EtablissementsController extends AbstractController
 {
-    #[Route('/etablissements', name: 'app_etablissements')]
+    #[Route('/etablissements', name: 'app_etablissements', methods: 'GET')]
     public function index(): Response
     {
-
-
         return $this->render('etablissements/index.html.twig', [
             'controller_name' => 'EtablissementsController',
+        ]);
+    }
+
+    #[Route('/etablissement/{id}', name: 'etablissement')]
+    public function etablissement($id): Response
+    {
+        return $this->render('etablissements/etablissement.html.twig', [
+            'id_etablissement' => $id,
         ]);
     }
 
