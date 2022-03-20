1a0f57ba2497ab52e56626314704f6e82bd18629
TEKFA FOUAD <fouad.tekfa@etu.univ-lehavre.fr>
2022-03-20 03:06:19 +0100

    ajout enity Commentaires

---
+++ src/Entity/Commentaires.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentairesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentairesRepository::class)]
+class Commentaires
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $auteur;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date_commentaire;
+
+    #[ORM\Column(type: 'text')]
+    private $commentaire;
+
+    #[ORM\Column(type: 'integer')]
+    private $note;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getDateCommentaire(): ?\DateTimeInterface
+    {
+        return $this->date_commentaire;
+    }
+
+    public function setDateCommentaire(\DateTimeInterface $date_commentaire): self
+    {
+        $this->date_commentaire = $date_commentaire;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
