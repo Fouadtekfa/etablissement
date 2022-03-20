e1ec989005cfaddfdb1eae73c8dd5093cdd25975
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-03-20 14:22:43 +0100

    suppretion d'array innecesaire

--- src/Controller/EtablissementsController.php
+++ src/Controller/EtablissementsController.php
@@ -19,37 +19,6 @@ class EtablissementsController extends AbstractController
         foreach($repostories as $cle => $re) {
             $re->date_ouverture = $re->date_ouverture->format('d/m/Y');
         }
-        
-        $etablissements = [
-            [
-                "id" => 1,
-                "appellation_officielle" => "CAO",
-                "denomination_principale" => "Denom",
-                "secteur" => "Agricole",
-                "latitude" => "23,405",
-                "longitude" => "39,56",
-                "adresse" => "Le havre",
-                "departement" => "Seine Maritime",
-                "commune" => "Jsp",
-                "region" => "Normandie",   
-                "academie" => "Normand",
-                "date_ouverture" => "22/02/2022"   
-            ],
-            [
-                "id" => 2,
-                "appellation_officielle" => "FGH",
-                "denomination_principale" => "Denom2",
-                "secteur" => "Aviateur",
-                "latitude" => "35,405",
-                "longitude" => "86,46",
-                "adresse" => "Caen",
-                "departement" => "Calvados",
-                "commune" => "com",
-                "region" => "Normandie",   
-                "academie" => "Mont",
-                "date_ouverture" => "10/01/2020"   
-            ]
-        ];
 
         return $this->render('etablissements/index.html.twig', [
             'etablissements' => $repostories
